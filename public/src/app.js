6766a98cb9f6f8c26b016d6adacf4d9ff7b4402a
Alex Stoicuta <alex.stoicuta@123formbuilder.com>
2019-10-18 15:01:38 +0300

    Added new custom element with shadow root

--- public/src/app.js
+++ public/src/app.js
@@ -1,7 +1,23 @@
 class CustomElement extends HTMLElement{
+    root;
+
     constructor() {
         super();
-        this.innerText = 'Welcome to Tech Talks 2019';
+
+        const header     = document.createElement('h2');
+        this.root        = this.attachShadow({mode: "closed"});
+        const data       = this.getAttribute('data-title');
+
+        header.innerText = data;
+        this.root.appendChild(header);
+    }
+
+    static get observedAttributes() {
+        return [ 'data-title' ];
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        console.warn(`Attribute ${name} has changed from: ${oldValue} to ${newValue}`);
     }
 }
 
